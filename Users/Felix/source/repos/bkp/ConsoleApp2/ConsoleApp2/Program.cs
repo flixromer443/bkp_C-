351d6bf8e67da8d81be78396d1b83707537da141
unknown <flixromer443@gmail.com>
2022-02-15 10:23:01 -0300

    console app 2

---
+++ Users/Felix/source/repos/bkp/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          TextWriter texto = new StreamWriter("prueba.txt");
+            texto.WriteLine("hola");
+            texto.Close();
+            Console.ReadKey();
+        }
+    }
+}
