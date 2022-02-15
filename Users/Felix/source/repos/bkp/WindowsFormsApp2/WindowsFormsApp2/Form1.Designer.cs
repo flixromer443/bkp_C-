43ed829aa0cde76e06d8983df2c7e63d18216f9a
unknown <flixromer443@gmail.com>
2022-02-15 10:17:40 -0300

    app 2

---
+++ Users/Felix/source/repos/bkp/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.aceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(293, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(286, 145);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(267, 26);
+            this.usuario.TabIndex = 1;
+            this.usuario.TextChanged += new System.EventHandler(this.usuario_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(184, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(144, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Contraseña";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(286, 193);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.PasswordChar = '*';
+            this.contraseña.Size = new System.Drawing.Size(267, 26);
+            this.contraseña.TabIndex = 4;
+            // 
+            // aceptar
+            // 
+            this.aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aceptar.Location = new System.Drawing.Point(301, 305);
+            this.aceptar.Name = "aceptar";
+            this.aceptar.Size = new System.Drawing.Size(198, 44);
+            this.aceptar.TabIndex = 5;
+            this.aceptar.Text = "Aceptar";
+            this.aceptar.UseVisualStyleBackColor = true;
+            this.aceptar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aceptar);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Button aceptar;
+    }
+}
+
---
+++ Users/Felix/source/repos/bkp/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(usuario.Text=="")
+            {
+                MessageBox.Show("Por favor ingrese su usuario");
+       
+            }
+            if (contraseña.Text == "")
+            {
+                MessageBox.Show("Por favor ingrese su contraseña");
+
+            }
+            else
+            {
+               
+                if(usuario.TextLength<4 || contraseña.TextLength<4)
+                {
+                    MessageBox.Show("Su usuario y/o contraseña son incorrectos");
+                }
+                else
+                {
+                    MessageBox.Show("Usted ha iniciado sesion correctamente");
+                    this.Close();
+
+                }
+               
+            }
+        }
+
+        private void usuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
