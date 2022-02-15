a255b0adcd2d48b88a5bd9ee4249204d91938df8
unknown <flixromer443@gmail.com>
2022-02-15 10:19:26 -0300

    app 3

---
+++ Users/Felix/source/repos/bkp/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ver = new System.Windows.Forms.Label();
+            this.hora = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ver
+            // 
+            this.ver.AutoSize = true;
+            this.ver.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ver.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.ver.Location = new System.Drawing.Point(272, 90);
+            this.ver.Name = "ver";
+            this.ver.Size = new System.Drawing.Size(290, 69);
+            this.ver.TabIndex = 1;
+            this.ver.Text = "hh:mm:ss";
+            this.ver.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // hora
+            // 
+            this.hora.Enabled = true;
+            this.hora.Tick += new System.EventHandler(this.hora_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(208, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 52);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Designed by Felix";
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(217, 251);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(326, 26);
+            this.input1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ver);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label ver;
+        private System.Windows.Forms.Timer hora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input1;
+    }
+}
+
